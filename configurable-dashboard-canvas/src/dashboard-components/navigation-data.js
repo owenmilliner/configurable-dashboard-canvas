4e6ddef0d67876bbc7f76a548e159d7d64be4eec
chathu2686 <chathu2686@hotmail.com>
2022-01-24 11:16:40 +0000

    Creation of navigation component

---
+++ configurable-dashboard-canvas/src/dashboard-components/navigation-data.js
@@ -0,0 +1,67 @@
+export const imgData = [
+  {
+    name: "pie-chart",
+    id: "<pie-chart></pie-chart>",
+    img: "../src/images/pie.png",
+  },
+  {
+    name: "area-chart",
+    id: "<area-chart></area-chart>",
+    img: "../src/images/area.png",
+  },
+  {
+    name: "line-chart",
+    id: "<line-chart></line-chart>",
+    img: "../src/images/line.png",
+  },
+  {
+    name: "column-chart",
+    id: "<column-chart></column-chart>",
+    img: "../src/images/column.png",
+  },
+  {
+    name: "funnel-chart",
+    id: "<funnel-chart></funnel-chart>",
+    img: "../src/images/funnel.png",
+  },
+  {
+    name: "areaspline-chart",
+    id: "<areaspline-chart></areaspline-chart>",
+    img: "../src/images/areaspline.png",
+  },
+  {
+    name: "bar-chart",
+    id: "<bar-chart></bar-chart>",
+    img: "../src/images/bar.png",
+  },
+  {
+    name: "boxplot-chart",
+    id: "<boxplot-chart></boxplot-chart>",
+    img: "../src/images/boxplot.png",
+  },
+  {
+    name: "bubble-chart",
+    id: "<bubble-chart></bubble-chart>",
+    img: "../src/images/bubble.png",
+  },
+  {
+    name: "heatmap-chart",
+    id: "<heatmap-chart></heatmap-chart>",
+    img: "../src/images/heatmap.png",
+  },
+  {
+    name: "polar-chart",
+    id: "<polar-chart></polar-chart>",
+    img: "../src/images/polar.png",
+  },
+  {
+    name: "pyramid-chart",
+    id: "<pyramid-chart></pyramid-chart>",
+    img: "../src/images/pyramid.png",
+  },
+  {
+    name: "treemap-chart",
+    id: "<treemap-chart></treemap-chart>",
+    img: "../src/images/tree.png",
+  },
+];
