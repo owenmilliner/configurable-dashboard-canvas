a62f3a77974a7c9ddb7ab83977b54b3c07b09415
owenmilliner <owenmilliner1@gmail.com>
2022-01-24 14:22:38 +0000

    BubbleChart component created.

--- configurable-dashboard-canvas/src/chart-components/ChartMiddleware.js
+++ configurable-dashboard-canvas/src/chart-components/ChartMiddleware.js
@@ -2,5 +2,6 @@ import { AreaChart } from "./AreaChart";
 import { AreaSplineChart } from "./AreaSplineChart";
 import { BarChart } from "./BarChart";
 import { BoxPlotChart } from "./BoxPlotChart";
+import { BubbleChart } from "./BubbleChart";
 
-export { AreaChart, AreaSplineChart, BarChart, BoxPlotChart };
+export { AreaChart, AreaSplineChart, BarChart, BoxPlotChart, BubbleChart };
