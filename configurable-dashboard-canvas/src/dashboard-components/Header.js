4c77e20df63369fa44f138a9821c82d4a6300522
chathu2686 <chathu2686@hotmail.com>
2022-01-25 21:38:11 +0000

    add grid visibility toggle option

--- configurable-dashboard-canvas/src/dashboard-components/Header.js
+++ configurable-dashboard-canvas/src/dashboard-components/Header.js
@@ -8,8 +8,8 @@ export class Header extends LitElement {
         color: white;
         background-color: #06152c;
         display: grid;
-        grid-template-areas: "app bin file logo";
-        grid-template-columns: 20vw 10vw auto 20vw;
+        grid-template-areas: "app toggle bin file logo";
+        grid-template-columns: 15vw 10vw 15vw auto 20vw;
         grid-template-rows: 100%;
       }
 
@@ -44,6 +44,30 @@ export class Header extends LitElement {
         margin: auto;
         width: 5vw;
       }
+
+      #header__toggle {
+        grid-area: toggle;
+        display: inline-block;
+        margin: auto;
+        width: 15vw;
+        height: 3vh;
+        margin: auto;
+        font-weight: 300;
+        font-size: 3vh;
+      }
+
+      #toggle__label {
+        font-size: 3vh;
+        width: 10vw;
+        justify-content: center;
+      }
+
+      #toggle__checkbox {
+        margin: auto;
+        width: 2vh;
+        height: 2vh;
+        color: white;
+      }
     `;
   }
 
@@ -68,9 +92,6 @@ export class Header extends LitElement {
     return html`
       <div id="header">
         <p id="header__app">CDC</p>
-        <h1 id="header__file">
-          <span id="file__client">${this._client} / </span>${this._file}
-        </h1>
         <img
           id="header__bin"
           src="../src/images/recyclebin.png"
@@ -78,6 +99,22 @@ export class Header extends LitElement {
           ondragover="allowDrop(event)"
           ondrop="remove(event)"
         />
+        <div id="header__toggle">
+          <label id="toggle__label" for="switch">Page Grid</label>
+          <input
+            type="checkbox"
+            id="toggle__checkbox"
+            name="switch"
+            min="0"
+            max="1"
+            value="1"
+            checked
+            onchange="toggleGrid()"
+          />
+        </div>
+        <h1 id="header__file">
+          <span id="file__client">${this._client} / </span>${this._file}
+        </h1>
         <img
           id="header__logo"
           src="../src/images/crisp_logo.png"
