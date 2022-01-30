0280af9dd198238024a21dfe61c78bac73b07ad5
Mark Golley <Mark.Golley@0649.local>
2022-01-30 20:23:13 +0000

    front page completed, app page needs to be sorted to scroll and be correctly placed

--- configurable-dashboard-canvas/src/App.js
+++ configurable-dashboard-canvas/src/App.js
@@ -9,37 +9,58 @@ import "./router-outlet";
 import "./pages/cdc/cdc.page";
 
 class App extends router(LitElement) {
-static get styles() {
-    return css`.link{
-      
-    }
-    
-    .nav-container {
-    display: flex;
-    justify-content: space-evenly;
-
-}.Landing {
-      text-align: center;
-      margin-left:1vw; 
-      margin-right:1vw; 
-      color:white;
-      background-color: #06152c;
-      font-family: "Josefin Sans", sans-serif;
-      }
-      ul {list-style-type:none}
-    `;
-  }
 
   static get styles() {
-    return [css`.Landing {
+    return [css`.nav-container {
+    /* display: flex;
+    justify-content: space-evenly; */
+    }
+    .wrapper{
+background-color: #06162c;
+    position: fixed;
+    top: 0;
+    width:100%;
+
+    }
+    .Landing {
       text-align: center;
       margin-left:1vw; 
       margin-right:1vw; 
       color:white;
-      background-color: #06152c;
-      font-family: "Josefin Sans", sans-serif;
+      font-family: whitney,sans-serif;
       }
-      ul {list-style-type:none}`];
+      ul {
+      list-style-type:none;
+      margin: 0;
+      padding: 0;
+      text-align: center;
+      margin-bottom:10%
+    }
+      h1{
+        padding-top:100px;
+      padding-bottom:10px;
+        font-size: 80px;
+    
+  font-weight:400;}
+      li {
+      display: inline-block;
+      margin: 0 10px;
+      border: 1px solid black;
+      padding: 5px;
+}
+h2{
+  font-weight: 300;
+  padding-bottom:75px
+}
+h3{
+  padding-top:100px
+}
+img{padding-top:30px;
+padding-bottom:30px;
+margin: auto;
+        margin-right: 1vw;
+        width: 5vw;
+        float: right;}`];
   }
   
   static get properties() {
@@ -86,19 +107,32 @@ this.landing = !this.landing
 
   render() {
     return this.landing ? 
-    html`<div class="Landing">
-      <h1>Welcome to the.... CDC</h1>
-      <h2>(Configurable Dashboard Canvas)</h2>
-      <h3>Created by - Crisple Clear</h3>
-      <h4>Contributors:</h4>
-        <ul>
-          <li>Owen Milliner</li>
-          <li>Tharaka Mantridge</li>
-          <li>Mark Golley</li>
-        </ul>
+    html`<div class="wrapper"><img
+          id="header__logo"
+          src="../src/images/crisp_logo.png"
+          alt="Crisp logo"
+        /></div>
+    
+    <div class="Landing">
+      <h1>CONFIGURABLE DASHBOARD CANVAS</h1>
+      <h2>This tool allows you to prepare reports using off the shelf charting web components to create an exportable report</h2>
+      <div class="nav-container">
+      <nav-link class="link" href="/cdc" onclick=${this.removeLanding()}>Launch Tool</nav-link>
+
+    <div style="position: absolute; width:100%; bottom:0; left:0 ">
+    
+    <h3>Created by - Crisple Clear</h3>
+            
+            <h4>Contributors:</h4>
+              <ul>
+                <li>Owen Milliner</li>
+                <li>Tharaka Mantrige</li>
+                <li>Mark Golley</li>
+              </ul>
+  </div>
+    
     </div>
-    <div class="nav-container">
-    <nav-link class="link" href="/cdc" onclick=${this.removeLanding()}>LAUNCH TOOL</nav-link>
+    
     </div>
       <router-outlet active-route=${this.route}>
         <cdc-page route="cdc"></cdc-page>
--- configurable-dashboard-canvas/src/components/navigation/nav-link.component.js
+++ configurable-dashboard-canvas/src/components/navigation/nav-link.component.js
@@ -10,25 +10,31 @@ export class NavLink extends navigator(LitElement) {
   }
   static get styles() {
     return css`
-      a {
-        margin: 5px;
-      }
+    
       .nav-link {
     text-decoration: none;
-    border: solid 2px black;
-    color: black;
-    padding: 5px;
-    margin-right: 5px;
-    border-radius: 5px;
+    border: solid 5px #4bd8d4;;
+        background-color: #4bd8d4;
+        color:#06152c;
+    font-family: whitney,sans-serif;
+    font-weight:lighter;
+    padding-left:65px;
+    padding-right:65px;
+    padding-top:20px;
+    padding-bottom:20px;
+    font-size: 60px;
+    
+    /* border-radius: 30px; */
     display: inline-block;
     text-align: center;
+    
   }
   slot {
     font-weight: bold;
   }
   .nav-link:hover {
-    color: #ffffff;
-    background-color: #000000;
+    color: white;
+    background-color: #06152c;
   }
     `;
   }
