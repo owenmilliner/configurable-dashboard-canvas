bb373dca8d1e9a21c02372d6ecbf0cfd5cc4f1c4
owenmilliner <owenmilliner1@gmail.com>
2022-02-01 11:31:04 +0000

    blue bin added.

--- configurable-dashboard-canvas/src/dashboard-components/Header.js
+++ configurable-dashboard-canvas/src/dashboard-components/Header.js
@@ -110,7 +110,7 @@ export class Header extends LitElement {
         </div>
         <img
           id="header__bin"
-          src="../src/images/bin.png"
+          src="../src/images/blue-bin.png"
           alt="Recycle bin"
           ondragover="allowDrop(event)"
           ondrop="remove(event)"
